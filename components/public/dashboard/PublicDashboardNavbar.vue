f900405c798bace7756cff2ecb7db7714aa7cd99
dead1ike <faithcreed@mail.ru>
2021-06-27 15:53:58 +0200

    configure project

---
+++ components/public/dashboard/PublicDashboardNavbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-navbar type="dark" variant="dark" class="sticky-top d-inline-flex">
+    <b-navbar-brand to="/">
+      <h3 class="my-4 font-weight-bolder" link-classes="text-black-50">MetalCorp</h3>
+    </b-navbar-brand>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item> </b-nav-item>
+        <b-nav-item> </b-nav-item>
+        <b-nav-item> </b-nav-item>
+      </b-navbar-nav>
+      <!-- Right aligned nav items -->
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'PublicDashboardNavbar',
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="h-100 w-100 overflow-auto d-flex flex-column">
+    <public-dashboard-navbar />
+    <Nuxt class="h-100 w-100 d-flex overflow-hidden container" style="max-width: 1600px" />
+    <div class="d-flex w-100 overflow-hidden justify-content-center"></div>
   </div>
 </template>
 
-<style></style>
+<style lang="scss">
+@import 'assets/scss/app';
+</style>
