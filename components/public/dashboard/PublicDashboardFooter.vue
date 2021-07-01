8d16127a159559107c239d07a7a70e0d07757060
dead1ike <faithcreed@mail.ru>
2021-07-01 09:45:43 +0200

    fix store

--- components/public/dashboard/PublicDashboardFooter.vue
+++ components/public/dashboard/PublicDashboardFooter.vue
@@ -4,22 +4,18 @@
     style="background-color: #1a1a1a"
   >
     <div class="border border-warning">
-      <div class="border-bottom h-100 border-light d-flex flex-fill flex-row justify-content-center">
-        <div class="border border-warning mx-5 d-flex flex-wrap">
-          <b-img
-            src="https://thumb.tildacdn.com/tild6536-3538-4137-b034-373731613935/-/resize/130x/-/format/webp/__2_2.png"
-          ></b-img>
-        </div>
-        <div class="d-flex flex-row border border-warning">
-          <div class="border border-warning mx-3 d-flex">ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ «МЕТАЛЛКОРП»</div>
-          <div class="border border-warning mx-3 d-flex">+7 (906) 812-81-75 info@m-lc.ru</div>
+      <div class="border-bottom">
+        <div class="border border-warning"></div>
+        <div class="border border-warning">
+          <div class="border border-warning">top text 1</div>
+          <div class="border border-warning">top text 2</div>
         </div>
       </div>
       <hr />
-      <div class="border-bottom h-100 border-light d-flex flex-fill flex-row justify-content-center">
-        <div class="mr-5 d-flex flex-row">
-          <div class="border border-warning ml-5 d-flex">ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ «МЕТАЛЛКОРП»</div>
-          <div class="border border-warning ml-5 d-flex">+7 (906) 812-81-75 info@m-lc.ru</div>
+      <div class="border-bottom border-light">
+        <div>
+          <div class="border border-warning">bottom text 1</div>
+          <div class="border border-warning">bottom text 2</div>
         </div>
       </div>
     </div>
@@ -31,3 +27,5 @@ export default {
   name: 'PublicDashboardFooter',
 }
 </script>
+
+<!--<b-img src="https://thumb.tildacdn.com/tild6536-3538-4137-b034-373731613935/-/resize/130x/-/format/webp/__2_2.png"/>-->
