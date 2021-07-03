0a5b63cfef3847ad5927649e90315fcc9413b9ed
Dearo <dearocosmocat@gmail.com>
2021-07-04 00:12:52 +0200

    change header ,and logo

--- components/public/dashboard/PublicDashboardNavbar.vue
+++ components/public/dashboard/PublicDashboardNavbar.vue
@@ -1,27 +1,27 @@
 <template>
-  <b-navbar toggleable="sm" type="dark" style="background: #5472f9">
+  <b-navbar toggleable="sm">
     <b-navbar-brand to="/">
-      <b-img src="~/static/logo.png" />
+      <b-img style="max-width: 150px; max-height: 63px" src="~/static/logo.png" />
     </b-navbar-brand>
     <b-navbar-toggle target="nav-collapse">
       <template #default="{ expanded }">
-        <b-icon v-if="expanded" icon="chevron-double-up" variant="white"></b-icon>
-        <b-icon v-else icon="chevron-double-down" variant="white"></b-icon>
+        <b-icon v-if="expanded" icon="chevron-double-up" variant="dark"></b-icon>
+        <b-icon v-else icon="chevron-double-down" variant="dark"></b-icon>
       </template>
     </b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
+        <div class="d-flex flex-column">
+          <b-nav-item href="tel:+79995601448" class="m-0 px-3 mt-2 font-weight-bolder text-truncate">
+            <h4>+7 (999) 560-14-48</h4>
+          </b-nav-item>
+          <small class="text-muted ml-5 mt-0">График работы: с 09:00 до 21:00</small>
+        </div>
         <b-nav-item to="/category" class="m-0 px-3 py-4 m-0 font-weight-bolder text-truncate">Каталог</b-nav-item>
         <b-nav-item to="/offer" class="m-0 px-3 py-4 m-0 font-weight-bolder text-truncate">
           <public-offer-basket />
         </b-nav-item>
-        <b-nav-item
-          href="https://metalcorp.org/"
-          target="_blank"
-          class="m-0 px-3 py-4 m-0 font-weight-bolder text-truncate"
-        >
-          O нас
-        </b-nav-item>
+        <b-btn class="text-truncate p-2 m-3" variant="corp" @click="openModal()"> Заказать звонок </b-btn>
       </b-navbar-nav>
       <!-- Right aligned nav items -->
     </b-collapse>
@@ -31,5 +31,13 @@
 <script>
 export default {
   name: 'PublicDashboardNavbar',
+  methods: {
+    openModal() {
+      this.$store.commit('setActiveModal', {
+        modalName: 'dimensionModal',
+        modalStatus: true,
+      })
+    },
+  },
 }
 </script>
--- components/public/dimension/PublicDimensionModal.vue
+++ components/public/dimension/PublicDimensionModal.vue
@@ -5,7 +5,7 @@
     </template>
     <div class="px-5">
       <h5 class="mb-5">Введите свой номер телефона и Вам поступит бесплатный звонок от нашего специалиста</h5>
-      <b-form-input class="p-2" v-model="form.phone" placeholder="+7(999)123-14-55"></b-form-input>
+      <b-form-input class="p-2" v-model="form.phone" placeholder="+7 (999) 123-14-55"></b-form-input>
     </div>
     <template #modal-footer>
       <div class="d-flex mr-md-2 flex-column justify-content-end flex-sm-row w-100">
