a1446e1ef90f7e77679926304d53c87a9ea69169
Mister Fil <mister-fil@yandex.com>
2021-07-05 09:21:06 +0200

    Fix link PublicDashboardControl

--- components/public/dashboard/PublicDashboardControl.vue
+++ components/public/dashboard/PublicDashboardControl.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="d-flex w-100 flex-row flex-wrap justify-content-around bg-light shadow">
-    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/rekvizity/">Реквизиты</b-btn>
-    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/otzyvy/">Отзывы</b-btn>
-    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/obyekty/">Наши объекты</b-btn>
-    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/vakansii/">Вакансии</b-btn>
-    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/kontakty/">Контакты</b-btn>
-    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/sotrudnichestvo/"
-      >Сотрудничество</b-btn
-    >
+    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/rekvizity">Реквизиты</b-btn>
+    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/otzyvy">Отзывы</b-btn>
+    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/obyekty">Наши объекты</b-btn>
+    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/vakansii">Вакансии</b-btn>
+    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/kontakty">Контакты</b-btn>
+    <b-btn variant="outline-corp" size="sm" class="m-2 font-weight-bold live-edit" to="/sotrudnichestvo">
+      Сотрудничество
+    </b-btn>
   </div>
 </template>
 
