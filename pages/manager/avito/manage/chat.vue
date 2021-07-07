078f73d2ad7bd1ba8d3bfab4bdb492fc3011ee73
Mister Fil <mister-fil@yandex.com>
2021-07-08 01:53:02 +0200

    Update Avito Chat

--- pages/manager/avito/manage/chat.vue
+++ pages/manager/avito/manage/chat.vue
@@ -10,7 +10,53 @@
         bordered
         hover
         no-border-collapse
-      ></b-table>
+      >
+        <template #cell(avito_user_id)="data">
+          <div v-if="data.item.avito_user">
+            <div>
+              <b-img :src="data.item.avito_user.avatar" thumbnail></b-img>
+            </div>
+            <div>
+              {{ data.item.avito_user.price_string }}
+            </div>
+            <div>
+              <a :href="data.item.avito_user.url" target="_blank">{{ data.item.avito_user.name }}</a>
+            </div>
+            <div>
+              {{ data.item.avito_user.location_title }}
+            </div>
+          </div>
+        </template>
+        <template #cell(avito_item_id)="data">
+          <div>
+            <div>
+              {{ data.item.avito_item.price_string }}
+            </div>
+            <div>
+              <a :href="data.item.avito_item.url" target="_blank">{{ data.item.avito_item.title }}</a>
+            </div>
+            <div>
+              {{ data.item.avito_item.location_title }}
+            </div>
+          </div>
+        </template>
+        <template #cell(last_message_content)="data">
+          <div class="d-flex flex-column">
+            <div class="">
+              {{
+                $DateTime
+                  .fromISO(data.item.last_message_created_at)
+                  .setLocale('ru')
+                  .toLocaleString($DateTime.DATETIME_SHORT)
+              }}
+              <hr />
+            </div>
+            <div>
+              {{ data.item.last_message_content.text }}
+            </div>
+          </div>
+        </template>
+      </b-table>
     </div>
     <div class="overflow-hidden text-center d-flex flex-row bg-light">
       <div class="flex-fill d-flex justify-content-center">
@@ -39,10 +85,7 @@ export default {
           key: 'avito_item_id',
         },
         {
-          key: 'last_message_content.text',
-        },
-        {
-          key: 'last_message_created_at',
+          key: 'last_message_content',
         },
         {
           key: 'actions',
