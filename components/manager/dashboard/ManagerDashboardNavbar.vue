f4fd3d47bd6b7d088c6c603723d1eb89c854ab9b
dead1ike <faithcreed@mail.ru>
2021-07-16 10:27:02 +0200

    fix visual

--- components/manager/dashboard/ManagerDashboardNavbar.vue
+++ components/manager/dashboard/ManagerDashboardNavbar.vue
@@ -9,9 +9,16 @@
     </b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item to="/manager/user/manage" class=""> Пользователи </b-nav-item>
-        <b-nav-item to="/manager/rack/manage" class=""> Стеллажи </b-nav-item>
-        <b-nav-item to="/manager/avito/manage/chat" class=""> Avito </b-nav-item>
+        <b-nav-item to="/manager/user/manage" class="mt-2 h5"> Пользователи </b-nav-item>
+        <b-nav-item to="/manager/rack/manage" class="mt-2 h5"> Стеллажи </b-nav-item>
+        <b-nav-item to="/manager/avito/manage/chat" class="mt-2 h5"> Avito </b-nav-item>
+        <b-nav-dropdown no-caret class="mt-2">
+          <template #button-content>
+            <b-icon icon="person-square" class="mb-1" variant="warning"></b-icon>
+            <span class="h5 ml-1">{{ $auth.user.email }}</span>
+          </template>
+          <b-dd-item @click="logout">Выход</b-dd-item>
+        </b-nav-dropdown>
       </b-navbar-nav>
       <!-- Right aligned nav items -->
     </b-collapse>
@@ -21,6 +28,12 @@
 <script>
 export default {
   name: 'ManagerDashboardNavbar',
+  methods: {
+    logout() {
+      this.$auth.logout()
+      this.$router.push('/manager/login')
+    },
+  },
 }
 </script>
 
