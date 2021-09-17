88d04752f61e4e5c6cb38708010c7dcd4d236988
dead1ike <faithcreed@mail.ru>
2021-09-17 11:09:45 +0200

    mass update

--- pages/product/_uuid/_slug.vue
+++ pages/product/_uuid/_slug.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="h-100 w-100 overflow-auto">
-    asd
+  <div class="h-100 w-100 overflow-auto good_card d-flex">
+    <div class="d-flex flex-column w-100">
+      <div class="p-4 d-flex">
+        <p class="good_card_title">{{ getGoodByUuid.title }}</p>
+      </div>
+      <div>
+        <b-img :src="getGoodByUuid.image" thumbnail />
+      </div>
+    </div>
+    <div class="d-flex flex-column w-100">
+      <div class="d-flex h-100 w-100 flex-column p-4">
+        <p class="pb-3 good_card_title">Описание</p>
+        <p class="good_card_description">
+          {{ getGoodByUuid.description }}
+        </p>
+      </div>
+      <hr />
+      <div class="d-flex h-100 w-100 flex-column p-4">
+        <p class="pb-3 good_card_title">Параметры</p>
+        <div class="d-flex flex-row justify-content-between" v-for="param in getGoodByUuid.good_parameters">
+          <span class="good_card_description">{{ param.title }}</span>
+          <span class="good_card_description">{{ param.parameter_value }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
