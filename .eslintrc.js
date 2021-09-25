59f339243db5829a4f722603e336072057abbef5
Mister Fil <mister-fil@yandex.com>
2021-09-25 14:55:55 +0200

    Update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,19 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    // parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
+  // extends: ['@nuxtjs', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {
-    'no-console': 0,
-    'comma-dangle': ['error', 'only-multiline'],
-  },
+  // rules: {
+  //   'no-console': 0,
+  //   'comma-dangle': ['error', 'only-multiline'],
+  // },
+  rules: {},
 
   globals: {
     _: true,
